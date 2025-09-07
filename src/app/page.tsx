68d56d3b068b3c0524875682e284c9383370c46a
diwashTiwari <tdiwash1534@gmail.com>
2025-09-07 21:24:56 +0545

    Initialized Project

---
+++ src/app/page.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+
+import { appData } from "@/data";
+import { BarPlot } from "@mui/x-charts/BarChart";
+import { ChartsGrid } from "@mui/x-charts/ChartsGrid";
+import { ChartsXAxis } from "@mui/x-charts/ChartsXAxis";
+import { ChartsYAxis } from "@mui/x-charts/ChartsYAxis";
+import { ChartsTooltip } from "@mui/x-charts/ChartsTooltip";
+import { LinePlot, MarkPlot } from "@mui/x-charts/LineChart";
+import { ChartContainer } from "@mui/x-charts/ChartContainer";
+import { ChartsLegend } from "@mui/x-charts/ChartsLegend";
+
+const newDataSet = appData.map(
+  ({ Total_kWhs_Used, Avg_Daily_Temperature, Date: date }) => ({
+    km: Total_kWhs_Used,
+    temp: Avg_Daily_Temperature,
+    date: new Date(date).toLocaleDateString("en-GB", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "2-digit",
+    }),
+  })
+);
+
+const series = [
+  {
+    type: "bar",
+    dataKey: "km",
+    color: "#00a35c",
+    yAxisId: "leftAxis",
+    label: "Total kWhs Used",
+    legendLabel: "Total kWhs Used",
+    marker: { shape: "square" },
+  },
+  {
+    type: "line",
+    dataKey: "temp",
+    color: "#ff0000",
+    yAxisId: "rightAxis",
+    label: "Average Temperature",
+    legendLabel: "Average Temperature",
+    marker: { shape: "circle" },
+  },
+] as const;
+
+export default function Home() {
+  return (
+    <div className="flex flex-1 items-center h-screen bg-white p-4 rounded-2xl">
+      <Stack sx={{ width: "100%" }}>
+        <Box sx={{ width: "100%", height: "500px" }}>
+          <ChartContainer
+            series={series}
+            xAxis={[
+              {
+                scaleType: "band",
+                dataKey: "date",
+                label: "Date",
+              },
+            ]}
+            yAxis={[
+              {
+                id: "leftAxis",
+                width: 80,
+                label: "kWh Used",
+                valueFormatter: (value: number) => value.toLocaleString(),
+              },
+              {
+                id: "rightAxis",
+                position: "right",
+                width: 80,
+                label: "Temperature (°C)",
+                valueFormatter: (value: number) => value.toFixed(1),
+              },
+            ]}
+            dataset={newDataSet}
+            height={500}
+            margin={{ top: 60, bottom: 80, left: 100, right: 100 }}
+          >
+            <ChartsLegend />
+            <ChartsGrid horizontal />
+            <BarPlot />
+            <LinePlot />
+            <MarkPlot />
+
+            <ChartsXAxis />
+            <ChartsYAxis axisId="leftAxis" />
+            <ChartsYAxis axisId="rightAxis" />
+            <ChartsTooltip />
+          </ChartContainer>
+        </Box>
+      </Stack>
+    </div>
+  );
+}
